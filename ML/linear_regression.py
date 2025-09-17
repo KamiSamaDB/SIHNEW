00837402d2cb4bdbc2d73e42884b4598f5ead44f
DebDev25 <bdesperado24@gmail.com>
2025-09-17 09:09:41 +0530

    added basic linear regression model for predictions

--- ML/linear_regression.py
+++ ML/linear_regression.py
@@ -4,8 +4,8 @@ from datetime import datetime, timedelta
 distances = np.array([1.0, 3.0, 3.1, 4.7, 5.5, 6.1])
 times = np.array([2.0, 4.0, 6.0, 8.0, 10.0, 12.0])
 
+# start time
 start_time = datetime.now()
-print(f"Journey started at: {start_time.strftime('%Y-%m-%d %H:%M:%S')}")
 
 N = len(distances)
 
@@ -23,6 +23,7 @@ predicted_times = m * distance_range + b
 distance_to_predict = 8.0
 pred_time_at_8km = m * distance_to_predict + b
 
+# arrival time
 arrival_time = start_time + timedelta(minutes=pred_time_at_8km)
 
 print(f"Expected arrival: {arrival_time.strftime('%Y-%m-%d %H:%M:%S')}")
